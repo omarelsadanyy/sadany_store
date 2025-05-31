9d4ca32fc65d1b686e0bf1538f0cb3bec5d0d660
omarelsadanyy <omarelsadany600@gmail.com>
2025-05-31 20:52:12 +0300

    env created

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sadany_store/core/app/env.variables.dart';
 import 'package:sadany_store/firebase_options.dart';
 import 'package:sadany_store/sadany_store_app.dart';
 
 void main() async{
    WidgetsFlutterBinding.ensureInitialized();
+
+   await EnvVarible.instance.init(envType: EnvTypeEnum.dev);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const SadanyStoreApp());
+
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown,
+  DeviceOrientation.portraitUp]).then((value){
+    runApp(const SadanyStoreApp());
+  });
+  
 }
